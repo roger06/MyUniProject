6cc421365f7cf9751979d591b2bc63d399b33fe7
roger06 <r.holden@chi.ac.uk>
2025-07-03 16:05:20 +0100

    initial

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+function Message() {
+  return (
+    <div>
+      <h1>Welcome to My Uni Project</h1>
+      <p>This is a simple message component.</p>
+    </div>
+  );
+}   
+export default Message;
+
+
